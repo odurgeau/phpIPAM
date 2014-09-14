857e1a138da60c9fbf2380f684728fb0ca8d4cbe
Benoit Benedetti <humboldtux@gmail.com>
2014-09-14 17:50:42 +0200

    Vagrant ok

--- install-phpipam.sh
+++ install-phpipam.sh
@@ -13,13 +13,13 @@ wget http://downloads.sourceforge.net/project/phpipam/phpipam-1.0.tar -O /tmp/ph
 tar xf /tmp/phpipam-1.0.tar --strip-components 1 -C /var/www/
 
 rm -rf /var/www/install/
-cp  /vagrant/config.php  /var/www/
 mysql -u root -e "CREATE DATABASE phpipam;"
-mysql -u root phpipam  < /var/www/db/SCHEMA.sql 
+mysql -u root phpipam  < /var/www/db/SCHEMA.sql
+mysql -u root  -e "grant ALL on phpipam.* to phpipam@localhost identified by 'phpipamadmin';"
+service mysql restart
 
 chown -R www-data: /var/www/
-chmod go-rwx /var/www/config.php
 
 a2enmod rewrite
-cp /vagrant/default /etc/apache2/sites-available/default
-service apache2 restart
+sed -i s/None/All/g /etc/apache2/sites-available/default
+service apache2 reload
